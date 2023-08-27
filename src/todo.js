e96421403971477713ba6c22a1c025b224ae93f0
vinothrobinson <vinothrobinson03@gmail.com>
2023-08-27 14:31:57 -0400

    Created a way to remove tasks from other projects and reflecting those changes in the projects

--- src/todo.js
+++ src/todo.js
@@ -1,13 +1,15 @@
-let taskList = [];
+import {projectList} from "./project";
+export let taskList = [];
 
 // Todo-List Item Constructor
 export class TodoItem
 {
-    constructor(title, description, dueDate, priority) {
+    constructor(title, description, dueDate, priority, projectName) {
         this.title = title;
 	    this.description = description;
 	    this.dueDate = dueDate;
 	    this.priority = priority;
+        this.projectName = projectName
     }
 
 }
@@ -21,8 +23,9 @@ const addNewTask = () => {
     let dueDate = document.querySelector("#due-date").value;
     if (dueDate === "") dueDate = "No Date";
     let priority = document.querySelector("#priority").value;
+    let projectName = "Home" // Use to distinguish between tasks from different projects
 
-    let newTodoItem = new TodoItem(title, description, dueDate, priority);
+    let newTodoItem = new TodoItem(title, description, dueDate, priority, projectName);
     taskList.push(newTodoItem);
     displayTask()
 }
@@ -48,9 +51,28 @@ const displayTask = () => {
     }
     let complete = document.querySelectorAll(".complete")
     complete.forEach(function callback(c, index) {
-        c.addEventListener("click", function() {
-            taskList.splice(index, 1)
-            displayTask()
+        c.addEventListener("click", function(event) {
+            if (taskList[index].projectName === "Home") {
+                taskList.splice(index, 1)
+                displayTask()
+            }
+            else { // Removes the task from the task list and project list
+                // console.log(event.target.closest(".task"))
+                // console.log(taskList[index])
+                let selectedTask = event.target.closest(".task")
+                selectedTask = (selectedTask.children[1])
+                selectedTask = (selectedTask.children[0])
+                // console.log(selectedTask)
+                let pIndex = projectList.findIndex(item => item.title === taskList[index].projectName);
+                // console.log(pIndex)
+                let currentList = projectList[pIndex].projectTasks
+                // console.log(currentList)
+                let currentTodoIndex = currentList.findIndex(item => item.title === selectedTask.innerHTML)
+                // console.log(currentList[currentTodoIndex])
+                taskList.splice(index, 1)
+                currentList.splice(currentTodoIndex, 1);
+                displayTask()
+            }
         })
     })
 }
